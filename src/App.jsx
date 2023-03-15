d63ad917d10660c8b0c741b46c3596b4de9fa60d
Chani Wolpo <chani.wolpo@gmail.com>
2023-03-16 01:03:25 +0200

    Mui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+import Navbar from './Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,4 +1,30 @@
-const FirstComponent = () => {
+import { Fragment } from "react";
+import Button from "@mui/material/Button";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import Fingerprint from "@mui/icons-material/Fingerprint";
+import { Chip, Divider } from "@mui/material";
 
-}
-export default FirstComponent
+
+const FirstComponent = () => {
+  const stameTaxt = "hhhhhhhh";
+  return (
+    <Fragment>
+      <h1 sx={{ fontsize: 500 }}>Hello Components </h1>
+      <Divider>
+      <Chip label="my chip"></Chip>
+      </Divider>
+      <h2 style={{ color: "red" }}>Hello {stameTaxt} </h2>
+      <Button variant="text" color="secondary">
+        click me
+      
+
+      <IconButton aria-label="fingerprint" color="secondary">
+        <Fingerprint />
+      </IconButton>
+      </Button>
+    </Fragment>
+  );
+};
+export default FirstComponent;
